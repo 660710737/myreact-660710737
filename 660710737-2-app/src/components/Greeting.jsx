b2a8adea010649ed87738b1840f7bfb8a55ff24f
660710737 <lakthan_v@su.ac.th>
2025-08-28 06:29:49 +0000

    week5-lab

---
+++ 660710737-2-app/src/components/Greeting.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Greeting = () => {
+    return (
+        <div className="greeting">
+            <h2>Hello React Developer! 👋</h2>
+            <p>Let's create the impressive thing together!!</p>
+        </div>
+    );
+};
+
+export default Greeting;
